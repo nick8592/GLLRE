35fc09fbec48bb5e0246f13fd79e9e8c3113874b
nick8592 <nickybai8592@gmail.com>
2023-06-11 22:32:53 +0800

    update main, add test

--- main.py
+++ main.py
@@ -1,48 +1,92 @@
 import os
 import math
+import numpy as np
 from PIL import Image
 
 
-def calc_LRE(image, x_pos, y_pos, n):
+def calc_LRE(image, width, height, region_size):
     """
     calculate local relative entropy (LRE) of a pixel (x_pos, y_pos) in a nxn neighborhood
     """
-    local_relative_entropy = 0
-    mean_pixel_value = calc_mean_pixel_value(image, x_pos, y_pos, n)
+    # Create an empty NumPy array to store the modified pixel values
+    modified_array = np.empty((height, width), dtype=np.uint8)
 
-    for i in range((-(n - 1) / 2), (n - 1) / 2):
-        for j in range((-(n - 1) / 2), (n - 1) / 2):
-            pixel_value = image.getpixel((x_pos + i, y_pos + j))
-            local_relative_entropy += pixel_value * abs(
-                math.log(pixel_value / mean_pixel_value)
-            )
-    return local_relative_entropy
+    # Iterate through each pixel in the image
+    for y in range(height):
+        for x in range(width):
+            # Calculate the region boundaries
+            left = max(0, x - region_size // 2)
+            upper = max(0, y - region_size // 2)
+            right = min(width, x + region_size // 2 + 1)
+            lower = min(height, y + region_size // 2 + 1)
 
+            # Extract the region from the image
+            region = image.crop((left, upper, right, lower))
 
-def calc_mean_pixel_value(image, x_pos, y_pos, n):
-    """
-    calculate mean gray level value of the pixels in the neighborhood
-    """
-    total_pixel_value = 0
-    for i in range((-(n - 1) / 2), (n - 1) / 2):
-        for j in range((-(n - 1) / 2), (n - 1) / 2):
-            total_pixel_value += image.getpixel((x_pos + i, y_pos + j))
-    mean_pixel_value = total_pixel_value / n ^ 2
-    return mean_pixel_value
+            # Calculate the mean of the region
+            region_mean = sum(region.getdata()) // (region_size * region_size)
+
+            # Get the pixel value at the current position
+            pixel_value = image.getpixel((x, y))
+
+            # Check if the pixel value or region mean is non-positive or zero
+            if pixel_value <= 0 or region_mean <= 0:
+                modified_array[y, x] = pixel_value
+            else:
+                # Perform the operations on the pixel value
+                result = math.log(abs(pixel_value / region_mean)) * pixel_value
+
+                # Normalize the result to the range of 0 to 255
+                normalized_value = max(0, min(255, result))
+
+                # Update the modified array with the normalized value
+                modified_array[y, x] = int(normalized_value)
+
+    # Save the modified pixel values as a grayscale image
+    modified_image = Image.fromarray(modified_array, mode='L')
+    modified_image.save('modified_image.png')
+
+    return modified_array
 
 
 # ===========================================================================
-folder_path = "/home/nick/Documents/dataset/COCO/coco2017/test2017"
+folder_path = "/Users/weichenpai/Dataset/coco2017/test2017"
 images_name_list = os.listdir(folder_path)
 images_path = [os.path.join(folder_path, name) for name in images_name_list]
 
 # Load the image
-image = Image.open(images_path[2])
+image = Image.open(images_path[5])
+
+# Get the width and height of the image
+width, height = image.size
+
+# Print the width and height
+print(f"Height: {height}")
+print(f"Width: {width}")
 
 # Convert the image to grayscale
 gray_image = image.convert("L")
+gray_arr = np.asarray(gray_image)
 
 image.show()
 gray_image.show()
 
-n = 2  # neighborhood
+n = 20  # neighborhood
+lre_arr = calc_LRE(gray_image, width, height, n)
+
+print(gray_arr.shape)
+print(lre_arr.shape)
+
+# Flatten the images to 1D arrays
+I_flat = gray_arr.flatten()
+J_flat = lre_arr.flatten()
+
+# Calculate nij, pixel pairs
+nij = np.zeros((256, 256), dtype=int)
+for i in range(height * width):
+    nij[I_flat[i], J_flat[i]] += 1
+
+# Calculate pij, occurence frequency
+pij = nij / (height * width)
+print(pij.shape)
+
